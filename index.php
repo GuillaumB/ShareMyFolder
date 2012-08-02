3f7ad349594f155d235714c5a142fe56e8f742d2
GuillaumB <bourbon.guillaume@gmail.com>
2012-08-02 20:47:08 +0200

    adding something

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <title>ShareMyFolder</title>
+  <link rel="icon" type="image/png" href="img/favicon/folder.png">
+
+  <!-- Style/CSS -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" media="all">
+  <link rel="stylesheet" type="text/css" href="css/index.css" media="all">
+
+  <!-- Script/JS -->
+  <script type="text/javascript" src="js/lib/angular.js"></script>
+</head>
+<body>
+  <header class="navbar navbar-fixed-top"><!-- Menu -->
+    <div class="navbar-inner">
+        <div class="container">
+          <a class="brand" href="#">ShareMyFolder</a>
+          <ul class="nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+          </ul>
+          <form class="navbar-search pull-right">
+            <input type="text" class="search-query" placeholder="Search">
+          </form>
+        </div>
+      </div>
+  </header>
+
+
+  <footer>
+    
+  </footer>
+
+  <!-- Script/JS -->
+  <script type="text/javascript" src="js/lib/jquery.js"></script>
+</body>
+</html>
