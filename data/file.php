1c1f81eb4e60ebfcc283d00cb6bdd261fc8e4efd
GuillaumB <bourbon.guillaume@gmail.com>
2012-08-09 23:10:17 +0200

    Work in Progress

--- data/file.php
+++ data/file.php
@@ -7,7 +7,7 @@ if(isset($_GET['folder'])){
   $folderName = $_GET['folder'];
 
   for($i=1;$i<4;$i++){
-    $tmp = array('name' => $folderName.$i, 'type' => 'folder');
+    $tmp = array('name' => $folderName.$i, 'type' => 'file');
     array_push($links, $tmp);
   }
 }
@@ -19,9 +19,6 @@ else{
     array_push($links, $tmp);
   }
 }
-
-
-
 
 $links = json_encode($links);
 
